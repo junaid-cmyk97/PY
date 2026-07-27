a4034a5b8d4ff534bb3f251f27a11cf5965c8e2e
J9 <syedjunaid78545@gmail.com>
2026-07-27 12:02:22 +0530

    Hangman project

--- MY_PROJECTS/HANGMAN.py
+++ MY_PROJECTS/HANGMAN.py
@@ -1,26 +1,22 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
-
 chosen_word = random.choice(word_list)
 print(chosen_word)
-
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 placeholder = ''
 for letter in chosen_word:
     placeholder += "_"
 print(placeholder)
-
 guess = input("Guess a letter: ").lower()
-
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-display = " "
 list_item = []
-for letter in chosen_word:
-    if letter == guess:
-       display +=letter
-       list_item.append(guess)
-    elif letter in list_item:
-        display += letter
-    else:
-        display += "_"
-print(display)
+while True:
+    display = " "
+    guess = input("Guess a letter: ").lower()
+    for letter in chosen_word:
+        if letter == guess:
+           display +=letter
+           list_item.append(guess)
+        elif letter in list_item:
+            display += letter
+        else:
+            display += "_"
+    print(display)
