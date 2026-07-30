fb2ea85e94eed242e39fe91cff6a4dd4008d75af
junaid <syedjunaidquadri77@gmail.com>
2026-07-30 14:56:33 +0530

    Update Coffe.py

--- CoffeeShop/Coffe.py
+++ CoffeeShop/Coffe.py
@@ -1,13 +1,13 @@
 class CoffeeMachine:
-    def __init__(self):
-        self.resources = {"water": 500,"milk": 300,"coffee": 100,"money": 0.0}
-        self.menu = {"espresso": {"water": 50, "milk": 0, "coffee": 18, "cost": 50.0},
+    def __init__(myself):
+        myself.resources = {"water": 500,"milk": 300,"coffee": 100,"money": 0.0}
+        myself.menu = {"espresso": {"water": 50, "milk": 0, "coffee": 18, "cost": 50.0},
                      "latte": {"water": 200, "milk": 150, "coffee": 24, "cost": 80.0},
                      "cappuccino": {"water": 250, "milk": 100, "coffee": 24, "cost": 100.0}
         }
-    def report(self):
+    def report(myself):
         print("\nMachine Report:")
-        for item, amount in self.resources.items():
+        for item, amount in myself.resources.items():
             if item in ["water", "milk"]:
                 unit = "ml" 
             elif item == "coffee":
@@ -16,12 +16,12 @@ class CoffeeMachine:
                 "₹"
             print(f"{item.capitalize()}: {amount}{unit}",)
         print()
-    def is_resource_sufficient(self, drink):
+    def is_resource_sufficient(myself, drink):
         for item in ["water", "milk", "coffee"]:
-            if self.resources[item] < self.menu[drink][item]:
+            if myself.resources[item] < myself.menu[drink][item]:
                 print(f"Sorry, not enough {item}.");return False
         return True
-    def process_payment(self, cost):
+    def process_payment(myself, cost):
         amount = float(input(f"Please insert ₹{cost:.2f}: "))
         if amount < cost:
             print("Sorry, that's not enough money. Money refunded.")
@@ -29,24 +29,24 @@ class CoffeeMachine:
         change = amount - cost
         if change > 0:
             print(f"Here is ₹{change:.2f} in change.")
-            self.resources["money"] += cost
+            myself.resources["money"] += cost
             return True
-    def make_coffee(self, drink):
+    def make_coffee(myself, drink):
         for item in ["water", "milk", "coffee"]:
-            self.resources[item] -= self.menu[drink][item]
+            myself.resources[item] -= myself.menu[drink][item]
         print(f"Here is your {drink} ☕. Enjoy!\n")
-    def run(self):
+    def run(myself):
         while True:
             choice = input("What would you like? (espresso/latte/cappuccino/report/off): ").lower()
             if choice == "off":
                 print("Turning off the coffee machine. Goodbye!")
                 break
             elif choice == "report":
-                self.report()
-            elif choice in self.menu:
-                if self.is_resource_sufficient(choice):
-                    if self.process_payment(self.menu[choice]["cost"]):
-                        self.make_coffee(choice)
+                myself.report()
+            elif choice in myself.menu:
+                if myself.is_resource_sufficient(choice):
+                    if myself.process_payment(myself.menu[choice]["cost"]):
+                        myself.make_coffee(choice)
             else:
                 print("Invalid choice. Please try again.\n")
 machine = CoffeeMachine();machine.run()
