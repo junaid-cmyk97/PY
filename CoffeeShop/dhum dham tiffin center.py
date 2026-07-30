93740482ac1ffc2ea18f98cb3d71f4dcfe56b45b
JD-pixel098 <bvbjagadeeshwar@gmail.com>
2026-07-30 10:18:13 +0530

    update

--- CoffeeShop/dhum dham tiffin center.py
+++ CoffeeShop/dhum dham tiffin center.py
@@ -1,15 +1,3 @@
-class shop:
-    def __init__(self,name):
-        self.name = DHUM_DHAM_TIFFIN_CENTER
-        self.menu = {"bonda_4 pieces ":20 ,
-                 "spl.bonda_8 pieces" : 22,
-                 "any dosa" :25
-        }
-        self.inventory = {"maida flour" :25,
-                          "gram flour":10,
-                          "rice flour":10
-        }
 
-import tensor
 
 
