34fe8c58dc3bb5abffb5e9f6f565d9433a579725
JD-pixel098 <bvbjagadeeshwar@gmail.com>
2026-09-07 16:11:30 +0530

    update

---
+++ JAGADEESH/MY_PROJECRS/to_/tkinter.py
@@ -0,0 +1,42 @@
+
+# import tkinter as tk
+#
+# import label
+#
+# prasad_root = tk.Tk()
+# prasad_root.geometry("733x434")
+# prasad_root.minsize("733x434")
+# prasad_root.maxsize("833x934")
+# label = tk.Label(text= "WELCOME TO PYCHARM")
+# prasad_root.title("Prasad")
+# prasad_root.mainloop()
+
+# import tkinter as tk                                 #1
+#
+# import label
+#
+# kd_root = tk.Tk()                                    #2
+#
+# kd_root.geometry("300x500")
+#
+# kd_root.minsize(100, 100)
+#
+# kd_root.maxsize(800, 700)
+#
+# label = tk.Label( text="Opening with 250 discount")
+# label.pack()
+#
+# kd_root.mainloop()                                      #3
+
+
+
+import tkinter as tk
+import label
+prasad_root = tk.Tk()
+prasad_root.title("welcome to pycharm")
+prasad_root.geometry("733x434")
+prasad_root.minsize(733,434)
+prasad_root.maxsize(933,634)
+label= tk.Label(text="welcome to pycharm")
+label.pack()
+prasad_root.mainloop()
