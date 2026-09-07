14e329c25081228d6a4993a8da144fa89378ae68
JD-pixel098 <bvbjagadeeshwar@gmail.com>
2026-09-07 15:17:15 +0530

    Update tkinter.py

--- JAGADEESH/MY_PROJECRS/tkinter.py
+++ JAGADEESH/MY_PROJECRS/tkinter.py
@@ -1,8 +1,31 @@
+# import tkinter as tk                                 #1
+#
+# import label
+#
+# kd_root = tk.Tk()                                    #2
+#
+# kd_root.geometry("300x500")
+#
+# kd_root.minsize(100, 100)
+#
+# kd_root.maxsize(800, 700)
+#
+# label = tk.Label( text="Opening with 250 discount")
+# label.pack()
+#
+# kd_root.mainloop()                                      #3
+
+
+
 import tkinter as tk
-
-root = tk.Tk()
-label = tk.Label(root, text="GeeksForGeeks.org!")
+import label
+prasad_root = tk.Tk()
+prasad_root.title("welcome to pycharm")
+prasad_root.geometry("733x434")
+prasad_root.minsize(733,434)
+prasad_root.maxsize(933,634)
+label= tk.Label(text="welcome to pycharm")
 label.pack()
+prasad_root.mainloop()
 
-root.mainloop()
 
